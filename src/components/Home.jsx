5fb0861ce7e31d22b8c830220a04dd2bc5830d44
Chandradeep Kumar <chandradeepkumar2002.ck@gmail.com>
2023-06-23 23:46:59 +0530

    changes in home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,42 +1,49 @@
-import React from 'react'
-import HeroImage from "../assets/heroImage.jpg"
-import { MdOutlineKeyboardArrowRight } from "react-icons/md"
-
+import React from "react";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import HeroImage from "../assets/heroImage.jpg";
+import { Link } from "react-scroll";
 
 const Home = () => {
   return (
-    <div name="home" className='justify-center h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
-       
-        <div className='max-w-screen-lg max-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
-
-            <div className='flex flex-col justify-center h-full'>
-
-                <h2 className='text-4xl sm:text-7xl font-bold text-white'>Hi , I am an Android Developer </h2>
-                <p className='text-gray-500 py-4 max-w-md'> I have Experience building and designing android applications .Currently my Tech Stack - Kotlin , Java , Node.js , Express.js , MYSQL 
-                    I am proficient in programming languages such as C++ , Java , C , Javascript </p>
-
-                    <div>
-                    <button className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'> Portfolio
-                        <span className='group-hover:rotate-90 duration-300'>
-                            
-                            <MdOutlineKeyboardArrowRight size={25} className='ml-1' />
-                            
-                        </span> 
-                </button>
-
-         </div>
-
-            </div>
-
-            <div>
-                <div>
-                    <img src={HeroImage} alt="my profile" className='justify-center rounded-2xl mx-auto w-2/3 md:w-full'/>
-                    </div> </div>
-
+    <div
+      name="home"
+      className="w-full h-screen bg-gradient-to-b from-black via-black to-gray-800"
+    >
+      <div className=" max-w-screen-lg mx-auto flex flex-col md:flex-row items-center justify-center h-full px-4">
+        <div className="flex flex-col justify-center h-full">
+          <h2 className="text-4xl sm:text-7xl font-bold text-white">
+            Hi , I'm an Android Developer
+          </h2>
+          <p className="text-gray-500 py-4 max-w-md">
+            I have Experience building and designing android applications
+            .Currently my Tech Stack - Kotlin , Java , Node.js , Express.js ,
+            MYSQL I am proficient in programming languages such as C++ , Java ,
+            C , Javascript
+          </p>
+          <div>
+            <Link
+              to="portfolio"
+              smooth
+              duration={500}
+              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+            >
+              Portfolio
+              <span className="group-hover:rotate-90 duration-300">
+                <MdOutlineKeyboardArrowRight className="ml-1" />
+              </span>
+            </Link>
+          </div>
         </div>
-
+        <div>
+          <img
+            src={HeroImage}
+            alt="my profile"
+            className="gravity-end rounded-4xl mx-auto w-2/5 md:w-full"
+          />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,90 +1,76 @@
-import React from 'react'
-import {FaGithub, FaLinkedin} from 'react-icons/fa'
-import {HiOutlineMail} from 'react-icons/hi'
-import {BsFillPersonLinesFill} from 'react-icons/bs'
-
+import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
 
 const SocialLinks = () => {
-
-
-    const links = [
-        {
-            id: 1,
-            child: (
-                <> 
-                    LinkedIn <FaLinkedin size={30} />
-                    </>
-            ),
-            href: "https://linkedin.com/chandradeepkumar16",
-            style: 'rounded-tr-md'
-
-        },
-        {
-            id: 2,
-            child: (
-                <> 
-                    Github <FaGithub size={30}/>
-                    </>
-            ),
-            href: "https://github.com/chandradeep16",
-            
-
-        },
-        {
-            id: 3,
-            child: (
-                <> 
-                    Mail <HiOutlineMail size={30}/>
-                    </>
-            ),
-            href: "mailto:chandradeepkumar2002.ck@gmail.com"
-
-        },
-        {
-            id: 4,
-            child: (
-                <> 
-                    Resume <BsFillPersonLinesFill size={30}/>
-                    </>
-            ),
-            href: "/resume.pdf",
-            style: 'rounded-br-md',
-            download: true
-
-        }
-
-
-
-    ]
-
-
-
+  const links = [
+    {
+      id: 1,
+      child: (
+        <>
+          LinkedIn <FaLinkedin size={30} />
+        </>
+      ),
+      href: "https://linkedin.com/in/chandradeepkumar16",
+      style: "rounded-tr-md",
+    },
+    {
+      id: 2,
+      child: (
+        <>
+          GitHub <FaGithub size={30} />
+        </>
+      ),
+      href: "https://github.com/chandradeepkumar16",
+    },
+    {
+      id: 3,
+      child: (
+        <>
+          Mail <HiOutlineMail size={30} />
+        </>
+      ),
+      href: "mailto:chandradeepkumar2002.ck@gmail.com",
+    },
+    {
+      id: 4,
+      child: (
+        <>
+          Resume <BsFillPersonLinesFill size={30} />
+        </>
+      ),
+      style: "rounded-br-md",
+      href: "/resume.pdf",
+      download: true,
+    },
+  ];
   return (
-    <div className='flex flex-col top-[35%] left-0 fixed'>
-        <ul>
-
-        {links.map((id ,child , href , style , download)=>(
-            <li key={id} className={"flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md bg-gray-500 duration-200"
-            + " " + style}>
-            
-            <a href={href} className='flex justify-between items-center w-full text-white'
-                download={download}
-                target='_blank'
-                rel="noreferrer"
+    <div className="hidden flex-col top-[35%] left-0 fixed lg:flex">
+      <ul>
+        {links.map((link) => (
+          <li
+            key={link.id}
+            className={
+              "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500" +
+              " " +
+              link.style
+            }
+          >
+            <a
+              href={link.href ? link.href : "/"}
+              className="flex justify-between items-center w-full text-white"
+              download={link.download}
+              target="_blank"
+              rel="noreferrer"
             >
-                {child}
-                
-                </a>
-        </li>
-
+              {link.child}
+            </a>
+          </li>
         ))}
-
-            
-        </ul>
-
-
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default SocialLinks
+export default SocialLinks;
